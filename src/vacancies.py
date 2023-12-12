02c93e12b6715dde3be8d4c3161f5d07cf6436fa
Dmitry Vasilenko <vasilenkoda91@gmail.com>
2023-12-13 00:59:16 +0300

    Add class Vacancy

---
+++ src/vacancies.py
@@ -0,0 +1,73 @@
+from typing import List, Any
+
+
+class Vacancy:
+    """Класс для вакансий."""
+    list_vacancies: List = []
+
+    def __init__(self, id_vacancy: int, town: str, name: str, url: str, salary: int, schedule: str, description: str):
+        """
+                Создание экземпляра класса Vacancy.
+                :param id_vacancy: id вакансии на сайте.
+                :param town: Город вакансии.
+                :param name: Наименование вакансии.
+                :param url: Ссылка на вакансию.
+                :param salary: Зарплата.
+                :param schedule: График работы.
+                :param description: Описание вакансии.
+                """
+        self.id_vacancy = id_vacancy
+        self.name = name
+        self.town = town
+        self.url = url
+        self.__salary = salary
+        self.schedule = schedule
+        self.description = description
+        self.list_vacancies.append(self)
+
+    @property
+    def salary(self) -> int:
+        return self.__salary
+
+    @salary.setter
+    def salary(self, salary: str | int) -> None:
+        if isinstance(salary, str) and '-' in salary:
+            list_salary = salary.split('-')
+            self.__salary = int(list_salary[1])
+        elif isinstance(salary, str) and '-' not in salary:
+            self.__salary = int(salary)
+        else:
+            self.__salary = int(salary)
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}({self.id_vacancy}, '{self.name}', {self.town}, {self.url}, {self.salary})"
+        )
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+    @classmethod
+    def instantiate_data(cls, list_vacancies: list) -> None:
+        """Классовый метод
+                Инициализирует экземпляры класса `Vacancy` данными из списка словарей list_vacancies
+                :param list_vacancies - списка словарей с данными вакансий
+                """
+        for vacancy in list_vacancies:
+            id_vacancy = vacancy['id_vacancy']
+            town = vacancy['town']
+            name = vacancy['name']
+            url = vacancy['url']
+            salary = vacancy['salary']
+            schedule = vacancy['schedule']
+            description = vacancy['description']
+            cls(id_vacancy, town, name, url, salary, schedule, description)
+
+    def __lt__(self, other: Any) -> Any:
+        return self.__salary < other.__salary
+
+    def __le__(self, other: Any) -> Any:
+        return self.__salary <= other.__salary
+
+    def __eq__(self, other: Any) -> Any:
+        return self.__salary == other.__salary
